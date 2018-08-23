80584e214f73b94aaf5dfcf18aa2d9debdb34cd9
daisuke_suzuki_edia <daisuke.suzuki.edia@gmail.com>
2018-08-23 11:02:08 +0200

    Initial Commit

---
+++ api_blueprint/gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+    aglio = require('gulp-aglio'),
+    browserSync = require('browser-sync'),
+    rename = require('gulp-rename'),
+    rimraf = require('rimraf'),
+    ejs = require('gulp-ejs');
+
+var reload = browserSync.reload;
+
+var TEMPLATE_FILES = ['src/apidocs/**/*.md'],
+    LAYOUT_FILE = 'src/apidocs/layout.md',
+    PUBLISHED_DIR = 'src/published';
+
+gulp.task('combine', function(){
+  return gulp.src(LAYOUT_FILE)
+    .pipe(ejs({},{ ext: '.md' }))
+    .pipe(rename('index.md'))
+    .pipe(gulp.dest(PUBLISHED_DIR));
+});
+
+gulp.task('generate-api-docs', ['combine'], function() {
+  return gulp.src(PUBLISHED_DIR + '/index.md')
+    .pipe(aglio({template: 'default'}))
+    .pipe(gulp.dest(PUBLISHED_DIR));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(TEMPLATE_FILES, ['generate-api-docs', reload]);
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    logConnections: true,
+    logFileChanges: true,
+    notify: true,
+    host: process.env.GULP_HOST,
+    port: 8088,
+    open: false,
+    server: {
+      baseDir: PUBLISHED_DIR
+    }
+  });
+});
+
+gulp.task('clean', function(cb) {
+  rimraf(PUBLISHED_DIR, cb);
+});
+
+gulp.task('publish', ['clean', 'generate-api-docs']);
+gulp.task('default', ['generate-api-docs', 'watch', 'browserSync']);
+gulp.task('default', ['generate-api-docs', 'watch', 'browserSync']);
